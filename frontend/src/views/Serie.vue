20097809080c053e6cd235f37fd18e3e933d2766
Romain Beaussart <romain.beaussart@efrei.net>
2019-10-22 14:09:45 +0200

    serie

---
+++ frontend/src/views/Serie.vue
@@ -0,0 +1,26 @@
+<template>
+  <container>
+        <v-progress-linear
+            color="red darken-2"
+            rounded
+            value="100"
+        ></v-progress-linear>
+  </container>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop, Watch } from "vue-property-decorator";
+
+import { Apollo } from "../decorators"
+
+@Component
+export default class Serie extends Vue {
+    get id() {
+        return this.$route.params.id;
+    }
+}
+</script>
+
+<style>
+
+</style>
